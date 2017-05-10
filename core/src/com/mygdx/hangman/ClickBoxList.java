93b9829c16fd0642474ec598e375a344e77775f0
Kanister10l <kamrep7@wp.pl>
2017-05-10 14:38:32 +0200

    Basic ClickBox

---
+++ core/src/com/mygdx/hangman/ClickBoxList.java
@@ -0,0 +1,31 @@
+package com.mygdx.hangman;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created on 10.05.2017 by Kamil Samul for usage in Hangman.
+ */
+public class ClickBoxList {
+    private List<ClickBox> clickBoxes;
+
+    public ClickBoxList(){
+        clickBoxes = new ArrayList<ClickBox>();
+    }
+
+    public void addClickBox(float fromX, float fromY, float toX, float toY){
+        clickBoxes.add(new ClickBox(fromX, fromY, toX, toY));
+    }
+
+    public void addClickBox(float fromX, float fromY, float toX, float toY, String nextView){
+        clickBoxes.add(new ClickBox(fromX, fromY, toX, toY, nextView));
+    }
+
+    public ClickResponse getResponse(float x, float y){
+        for (ClickBox clickBox: clickBoxes) {
+            if (clickBox.isClicked(x, y))
+                return clickBox.respond();
+        }
+        return null;
+    }
+}
