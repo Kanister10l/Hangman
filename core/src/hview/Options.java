299bfbbff1ff3e362a3376caa431718da0a0e51e
Dryja <hubertdryja@gmail.com>
2017-05-28 23:13:18 +0200

    options menu finished

--- core/src/hview/Options.java
+++ core/src/hview/Options.java
@@ -25,6 +25,7 @@ public class Options extends HView {
     private BitmapFont font1;
     private ShapeRenderer shapeRenderer;
     private Texture quitTexture;
+    private Boolean dzwiek=true;
     public Options(float h, float w) {
         name = "Options";
         this.h = h;
@@ -44,6 +45,8 @@ public class Options extends HView {
             /*Do something here according to operationCode*/
             if (operationCode == 1)
                 System.out.println("X: " + x + " Y: " + y);
+            if (operationCode == 2)
+                dzwiek = !dzwiek;
         }
         return clickResponse;
     }
@@ -54,7 +57,7 @@ public class Options extends HView {
         shapeRenderer = new ShapeRenderer();
         clickBoxList = new ClickBoxList();
         clickBoxList.addClickBox(w-100, h-100, w-50, h-50, "Menu");
-
+        clickBoxList.addClickBox(w / 4, h - 200, w / 4 + w / 2, h - 100 + 70, 2);
         font1 = new FontGenerator().getFont("Roboto-Medium.ttf",28,true, Color.WHITE);
         quitTexture = new Texture(Gdx.files.internal("core/assets/sprites/exit.png"));
     }
@@ -63,8 +66,20 @@ public class Options extends HView {
     public void render() {
         Gdx.gl.glClearColor(251/255f, 250/255f, 250/255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(46/255f, 38/255f, 33/255f, 1);
+        shapeRenderer.rect(w / 4, h - 200, w / 2, 70);
+        shapeRenderer.end();
+
+
 
         batch.begin();
+
+        if(dzwiek){
+            font1.draw(batch, "Wyłącz dźwięki", w / 4+w / 6, h - 150);
+        }else{
+            font1.draw(batch, "Włącz dźwięki", w / 4+w / 6, h - 150);
+        }
         batch.draw(quitTexture,w-100,h-100,50,50);
         batch.end();
 
